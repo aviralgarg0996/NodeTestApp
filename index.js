918634682d32ca9cf673cfdb62eeedcfa97ba111
Kartik2811 <kartiktandon@innotical.com>
2018-08-03 17:00:42 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express=require('express');
+const passport=require('passport');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const keys=require('./config/keys');
 const app=express();
 
-app.get('/',(req,res)=>{
-res.send({'hi':'there'})
-});
+passport.use(new GoogleStrategy({
+    clientID:keys.googleCliendID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'google/callback'
+},(accessToken)=>{
+console.log(accessToken)
+}));
 
-const PORT=process.env.PORT || 5000
-app.listen(PORT);
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}));
+// app.get('/',(req,res)=>{
+// res.send({'hi':'there'})
+// }); 
+app.get('auth/google/callback',passport.authenticate('google'))
+const PORT=process.env.PORT || 5000;
+app.listen(5000);
